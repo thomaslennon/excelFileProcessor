819f98bc8224e7a8799a626cac01a3205dcb3e18
Thomas Lennon <thomas.lennon@ericsson.com>
2015-07-09 17:22:34 +0100

    initial files

--- src/main/java/com/lennon/tom/App.java
+++ src/main/java/com/lennon/tom/App.java
@@ -2,12 +2,16 @@ package com.lennon.tom;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        String sourceFile = args[0];
+        String sourceColumn = args[1];
+        String compareFile = args[2];
+        String compareColumn = args[3];
+
+        new ExcelMerger(sourceFile, sourceColumn, compareFile, compareColumn);
+
     }
 }
---
+++ src/main/java/com/lennon/tom/ExcelMerger.java
@@ -0,0 +1,15 @@
+package com.lennon.tom;
+
+/**
+ *
+ */
+public class ExcelMerger {
+
+    public ExcelMerger(String file1, String column1, String file2, String column2){
+
+
+
+    }
+
+
+}
